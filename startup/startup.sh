142911f3f7cc115d021c519b9e240e6907033e89
Stanley Fang <stanleysfang92@gmail.com>
2020-04-06 14:47:19 -0700

    updated startup files

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
 #### Environment ####
-
-project="stanleysfang"
-
-repository="gcp_audit"
+repo="gcp_audit"
+repo_path=/home/sfang/windows/gitlab/stanleysfang/${repo}/
 
 #### startup ####
+conda remove -y --name ${repo} --all
 
-# Install Anaconda
-wget "https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh"
-bash $HOME/Anaconda3-5.2.0-Linux-x86_64.sh -b
-echo -e "\n# added by Anaconda3 installer\nexport PATH=\"$HOME/anaconda3/bin:\$PATH\"" >> $HOME/.bashrc # this doesn't work with airflow
-rm $HOME/Anaconda3-5.2.0-Linux-x86_64.sh
-export PATH="$HOME/anaconda3/bin:$PATH"
+conda create -y --name ${repo} python=3.7.0 pip
+source activate ${repo}
+pip install -r ${repo_path}/startup/requirements.txt
 
-# Create Conda Environment
-conda create -y --name gcp_audit python=3.7.0 pip
-source activate gcp_audit
-pip install -r $HOME/${repository}/startup/requirements.txt
+python -m ipykernel install --name ${repo} --display-name "Python 3.7.0 (${repo})" --user
